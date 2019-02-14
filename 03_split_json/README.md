8137ee299ea7c44e6d2a118140b2266b3155e514
Braulio Diez Botella <braulio.diez@gmail.com>
2019-02-14 13:58:03 +0100

    wip

---
+++ 03_split_json/README.md
@@ -0,0 +1,21 @@
+# Split json  
+
+Embedding all the translations in a js file and loading all at application startup may not
+be ideal for medium and big sized applications.
+
+In this sample we will split the translations files in separted json files, and we will 
+load them on deman.
+
+# Steps
+
+- Let's taking as starting point sample 02
+
+```
+npm install
+```
+
+*** WIP
+
+https://www.i18next.com/how-to/add-or-load-translations
+
+https://github.com/i18next/i18next-xhr-backend
---
+++ 03_split_json/src/i18n.ts
@@ -0,0 +1,42 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+// the translations
+// (tip move them in a JSON file and import them)
+const resources = {
+  en: {
+    translation: {
+      login: "Login",
+      "Invalid login or password, please type again":
+        "Invalid login or password, please type again",
+      "error, review the fields": "error, review the fields rrr",
+      "login plus username": "Usuario: {{username}}",
+      REQUIRED: "Mandatory field"
+    }
+  },
+  es: {
+    translation: {
+      login: "Introduzca credenciales",
+      "Invalid login or password, please type again":
+        "Usuario o clave no validos, porfavor intentelo de nuevo",
+      "error, review the fields": "Error, revise los campos por favor",
+      "login plus username": "Usuario: {{username}}",
+      REQUIRED: "Campo obligatorio"
+    }
+  }
+};
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .init({
+    resources,
+    lng: "es",
+
+    keySeparator: false, // we do not use keys in form messages.welcome
+
+    interpolation: {
+      escapeValue: false // react already safes from xss
+    }
+  });
+
+export default i18n;
